028f12559d0089ea88cfd1d2333712d49c23faf9
Shun Yao <ysmiracle@hotmail.com>
2018-03-12 04:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
-  
+  var arr = [];
+  for(var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
 }
+
+function 
