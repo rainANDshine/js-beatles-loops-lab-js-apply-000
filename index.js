4a0fa78a99ea35c47c6db0a8c229443979b459fe
Shun Yao <ysmiracle@hotmail.com>
2018-03-12 05:03:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var arr = [];
   do {
-    
-  }
+    arr.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
 }
